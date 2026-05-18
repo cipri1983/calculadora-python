c149316661f3091f2cca33b0f739a02ef3ba4334
Ciprian <constantinciprian1983@gmail.com>
2026-05-18 11:49:18 +0200

    Agrego calculadora con funciones

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+def sumar(a, b):
+    return a + b
+def restar(a, b):
+    return a - b
+def multiplicar(a, b):
+    return a * b
+def dividir(a, b):
+    if b == 0:
+        return "Error: no se puede dividir entre cero"
+    return a / b
+
+print("Calculadora de Ciprian")
+print(sumar(10, 5))
+print(restar(10, 5))
+print(multiplicar(10, 5))
+print(dividir(10, 5))
